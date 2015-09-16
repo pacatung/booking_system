c1ad30fe0cf6db28a3fff6c36f902c3d72bc1aad
BigBoss <pacatung@gmail.com>
2015-09-16 18:57:21 +0800

    bags index

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -1,2 +1,34 @@
 class BagsController < ApplicationController
+
+  def index
+    @bags = Bag.all
+    #@bags = Bag.all.order("id DESC")
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private
+
 end
