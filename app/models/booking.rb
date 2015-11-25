1a1132f2e0475ddc7215f321748049ab54ed9bb0
BigBoss <pacatung@gmail.com>
2015-11-25 22:45:51 +0800

    delete vaildate

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,4 @@
 class Booking < ActiveRecord::Base
-  validates_presence_of :lender, :phone, :identify_id, :home_address, :destination, :pickup_date, :return_date
   validates_numericality_of :phone
 
   belongs_to :user
